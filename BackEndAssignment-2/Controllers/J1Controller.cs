ab940b073ea18aec3a6ce33da65e86508777b118
Himanshu <himanshu.a.04.1999@gmail.com>
2024-09-30 11:15:23 -0400

    Initial commit

---
+++ BackEndAssignment-2/Controllers/J1Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndAssignment_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class J1Controller : ControllerBase
+    {
+        [HttpGet]
+
+        public string Get() {
+            return "hi";
+        }
+    }
+}
